2c7fbb33d83b5eaf5df5186dc4aa872dc2aedcc4
Mike Pham <opensource@nativecode.com>
2018-12-03 10:43:43 -0500

    fix: adding node project

--- src-tools/node/Options/NodeOptions.cs
+++ src-tools/node/Options/NodeOptions.cs
@@ -1,14 +1,7 @@
-﻿namespace node.Options
+namespace node.Options
 {
-    using System.Collections.Generic;
-
     public class NodeOptions
     {
         public string Name { get; set; } = Program.Name;
-
-        /// <summary>
-        /// Gets or sets the services.
-        /// </summary>
-        public List<NodeServiceOptions> Services { get; set; } = new List<NodeServiceOptions>();
     }
 }
